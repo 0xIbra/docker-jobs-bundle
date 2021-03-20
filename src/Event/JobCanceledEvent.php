5625b999e7fbf38c89a5f64e667dc2da8266eeac
Ibra <ibragim.ai95@gmail.com>
2021-03-21 00:38:39 +0100

    Symfony events removing deprecated code

--- src/Event/JobCanceledEvent.php
+++ src/Event/JobCanceledEvent.php
@@ -3,7 +3,7 @@
 namespace Polkovnik\DockerJobsBundle\Event;
 
 use Polkovnik\DockerJobsBundle\Entity\BaseJob;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 class JobCanceledEvent extends Event
 {
